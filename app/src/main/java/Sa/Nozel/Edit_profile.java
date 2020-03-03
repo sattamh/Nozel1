76bbe830d6c9b0f0a5cd52eeca8bd51105bcb938
sattamh <kk2145@yahoo.com>
2020-03-04 00:56:22 +0300

    Added New Layouts

---
+++ app/src/main/java/Sa/Nozel/Edit_profile.java
@@ -0,0 +1,14 @@
+package Sa.Nozel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile_lt);
+    }
+}
---
+++ app/src/main/java/Sa/Nozel/login.java
@@ -0,0 +1,14 @@
+package Sa.Nozel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_lt);
+    }
+}
